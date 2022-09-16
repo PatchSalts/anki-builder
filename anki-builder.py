90308922309eff9184958da71fa1908dbd0e39c5
Patch Salts <patchsalts@gmail.com>
2022-09-16 15:00:32 -0400

    Cleanup.

--- anki-builder.py
+++ anki-builder.py
@@ -3,11 +3,9 @@
 import sys
 import find_j
 
-print(str(len(sys.argv)) + " args: " + str (sys.argv))
-
 for filename in sys.argv[1:]:
     file = open(filename)
     for line in file:
-        for j in find_j.find_japanese(line):
-            print(j)
+        for fragment in find_j.find_japanese(line):
+            print(fragment)
     file.close()
